7ba7dbbfc5c86672ca3a3ac73724974a0840665a
Anita Wangui <anitawangui22@gmail.com>
2024-09-26 15:38:03 +0300

    write functions with conditions

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let message;
+  if (distance > 2500){
+    message = 'No can do.';
+  }
+  else if(distance <= 400){
+   message= 'This one is on me!';
+  }
+  else if(distance > 400 && distance <= 2000){
+    message= "That will be twenty bucks.";
+  }
+  else if(distance > 2000 ){
+    message="I will gladly take your thirty bucks.";
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
-}
+  return city === 'NYC'? 'Ok, sounds good.': 'No go.';
+  }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip==="generous")
+    {
+      return "Thank you so much.";
+    }
+  else if (tip==="not as generous"){
+    return "Thank you.";
+  }
+  else{
+    return "Bye.";
+  }
 }
